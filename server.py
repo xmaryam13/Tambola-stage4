1cecb55cbeef965efcc0a977de5270be07367011
xmaryam13 <90093129+xmaryam13@users.noreply.github.com>
2022-09-05 21:38:33 +0100

    Create server.py

---
+++ server.py
@@ -0,0 +1,43 @@
+import socket
+from threading import Thread
+
+SERVER = None
+IP_ADDRESS = '127.0.0.1'
+PORT = 6000
+
+CLIENTS = {}
+
+def setup():
+    print('\n\t\t\t\t\t*** Welcome to the Tambola Game***\n')
+    
+    global SERVER
+    global PORT
+    global IP_ADDRESS
+    
+    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.bind((IP_ADDRESS,PORT))
+    
+    SERVER.listen(10)
+    
+    print("\t\t\t\tSERVER IS WAITING FOR INCOMMING CONNECTIONS...\n")
+    
+def acceptConnection():
+    global CLIENTS
+    global SERVER
+    
+    while True:
+        player_socket, addr = SERVER.accept()
+        player_name = player_socket.recv(1024).decode().strip()
+        print(player_name)
+        
+        if(len(CLIENTS.keys()) == 0):
+            CLIENTS[player_name] = {'player_type' : 'player1'}
+        else:
+            CLIENTS[player_name] = {'player_type' : 'player2'}
+        
+        CLIENTS[player_name]['player_scoket'] = player_socket
+        CLIENTS[player_name]['address'] = addr
+        CLIENTS[player_name]['player_name'] = player_name
+        CLIENTS[player_name]['turn'] = False
+        
+        print(f"Connection established with {player_name} : {addr}")
